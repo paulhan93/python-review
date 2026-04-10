56764f232a0e44691866a3bc381270c68bbcb584
Paul Han <paul.han0822@gmail.com>
2026-04-10 00:12:37 -0700

    Fix awkward print output

--- week1/guessing_game.py
+++ week1/guessing_game.py
@@ -7,24 +7,26 @@ import random
 def main():
     num = random.randint(0,100)
 
-    print("Welcome to the number guessing game! \n \
-            You have 8 chances to guess the number. \n \
-            The number is between 0 and 100 (inclusive). \n \
-            Good luck!")
+    print("""
+    Welcome to the number guessing game! \n
+    You have 8 chances to guess the number. \n
+    The number is between 0 and 100 (inclusive). \n
+    Good luck!
+          """)
 
     for i in range (1,9):
         x = int(input(f"Guess #{i}: "))
         if x == num:
             print(f"Good job! You got it!")
             return
-        else:
-            if i == 8:
-                print(f"Noooo, the number was {num} :(")
-                return
+        elif i < 8:
             if x > num:
                 print(f"Try again, the number is lower!")
-            if x < num:
+            else:
                 print(f"Try again, the number is higher!")
+
+    print(f"Noooo, the number was {num} :(")
+
 
 if __name__ == "__main__":
     main()
