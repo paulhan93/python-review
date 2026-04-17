ec8a0aa6cc47ee4adb71ded05397fdc36c248fb5
Paul Han <paul.han0822@gmail.com>
2026-04-16 23:47:28 -0700

    complete triangle exercise

--- exercism/python/triangle/triangle.py
+++ exercism/python/triangle/triangle.py
@@ -1,10 +1,26 @@
-def equilateral(sides):
-    pass
+def isTriangle(sides):
+    a, b, c = sides
+    if a <= 0 or b <= 0 or c <= 0:
+        return False    # not a Triangle, negative or 0 length side(s)
+    if a + b > c and b + c > a and a + c > b:
+        return True
+    return False
 
+def equilateral(sides):
+    if not isTriangle(sides):
+        return False
+    a, b, c = sides
+    return a == b and b == c
 
 def isosceles(sides):
-    pass
+    if not isTriangle(sides):
+        return False
+    a, b, c = sides
+    return a == b or b == c or a == c
 
 
 def scalene(sides):
-    pass
+    if not isTriangle(sides):
+        return False
+    a, b, c = sides
+    return a != b and b != c and a != c
