c0ca31a4ea99fcf4513c5ba28bf80b8ec5520e29
Paul Han <paul.han0822@gmail.com>
2026-04-17 01:13:37 -0700

    armstrong_numbers exercise complete

--- exercism/python/armstrong-numbers/armstrong_numbers.py
+++ exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,11 @@
-def is_armstrong_number(number):
-    pass
+def is_armstrong_number(number) -> bool:
+    temp = number
+    digits = []
+    while temp > 0:
+        digit = temp % 10
+        digits.append(digit)
+        temp = temp // 10
+
+    power = len(digits)
+    armstrong_number = sum([x ** power for x in digits])
+    return number == armstrong_number
