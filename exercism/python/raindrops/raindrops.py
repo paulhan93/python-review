34b41ad656415e162c16a3468d37b3a79fd505fd
Paul Han <paul.han0822@gmail.com>
2026-04-20 20:30:21 -0700

    complete raindrops exercise

--- exercism/python/raindrops/raindrops.py
+++ exercism/python/raindrops/raindrops.py
@@ -1,2 +1,11 @@
 def convert(number):
-    pass
+    output: str = ''
+    if number % 3 == 0:
+        output += 'Pling'
+    if number % 5 == 0:
+        output += 'Plang'
+    if number % 7 == 0:
+        output += 'Plong'
+    if output == '':
+        return str(number)
+    return output
