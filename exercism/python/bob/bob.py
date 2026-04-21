84c050c5e7af66a8a3c213a34a92eb7eb7c39f56
Paul Han <paul.han0822@gmail.com>
2026-04-20 19:45:11 -0700

    complete bob exercise

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -1,2 +1,14 @@
 def response(hey_bob):
-    pass
+    is_yelling = hey_bob.upper() == hey_bob and any(c.isalpha() for c in hey_bob)
+    is_question = hey_bob.strip().endswith("?")
+
+    if hey_bob.strip() == '':
+        return "Fine. Be that way!"
+    elif is_yelling and is_question:
+        return "Calm down, I know what I'm doing!"
+    elif is_yelling:
+        return "Whoa, chill out!"
+    elif is_question:
+        return "Sure."
+    else:
+        return "Whatever."
