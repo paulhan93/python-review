ea02820c791d660e08607cc0008ee00b150801e4
Paul Han <paul.han0822@gmail.com>
2026-04-20 19:18:37 -0700

    complete collatz_conjecture exercise

--- exercism/python/collatz-conjecture/collatz_conjecture.py
+++ exercism/python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,14 @@
 def steps(number):
-    pass
+    # check for valid input; raise exception if not
+    if not number > 0: 
+        raise ValueError("Only positive integers are allowed")
+
+    count: int = 0
+    while True:
+        if number == 1:
+            return count
+        elif number % 2 == 0:
+            number //= 2
+        else:
+            number = (3 * number) + 1
+        count += 1
